589424031d344ce4272bf2dd5b4b7a33b88b2763
xavi-04 <chavidelcid@gmail.com>
2020-01-14 20:39:23 -0600

    Add: observations of instructor

--- Laboratorio 02/CW-01.cpp
+++ Laboratorio 02/CW-01.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
-float array[20];
+int main(void)
+{
+    float array[20];
 
-for(int i = 0; i < 20; i++){
-cout << " Digite la nota: "; cin >> array[i];
-}
+    for (int i = 0; i < 20; i++)
+    {
+        cout << " Digite la nota: ";
+        cin >> array[i];
+    }
 
-for(int = 0; i < 20; i++){
-    cout << "Nota " << (i + 1) << ":\t";
-}
-//???
-    return 0;
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "Nota " << (i + 1) << ":\t";
+    }
+    
 }
